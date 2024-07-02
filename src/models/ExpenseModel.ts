519be944583981ce7230ed8ead5252a7d5cc48e8
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-02 10:07:39 +0530

    Models added

---
+++ src/models/ExpenseModel.ts
@@ -0,0 +1,44 @@
+import { Schema, model, models } from "mongoose";
+
+const ExpenseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "expense",
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Expense = models.Expense || model("Expense", ExpenseSchema);
+export default Expense;
---
+++ src/models/IncomeModel.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+const IncomeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "income",
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Income = models.Income || model("Income", IncomeSchema);
+export default Income;
